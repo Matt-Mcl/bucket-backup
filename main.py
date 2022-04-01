940076aa778ea1e72ecc3c35110fd69ad19ac5a8
Matt-Mcl <mattmcl2000@gmail.com>
2022-04-01 20:22:41 +0100

    Removes oci package

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import boto3
-import oci
+from configparser import ConfigParser
 
 namespace = sys.argv[1]
 region = sys.argv[2]
@@ -9,12 +9,13 @@ bucket_name = sys.argv[3]
 bucket_dir = sys.argv[4]
 file = sys.argv[5]
 
-config = oci.config.from_file("~/.aws/credentials", profile_name="default")
+config = ConfigParser()
+config.read(os.path.expanduser("~/.aws/credentials"))
 
 s3 = boto3.client(
     "s3",
-    aws_access_key_id=config["aws_access_key_id"],
-    aws_secret_access_key=config["aws_secret_access_key"],
+    aws_access_key_id=config.get("default", "aws_access_key_id"),
+    aws_secret_access_key=config.get("default", "aws_secret_access_key"),
     region_name=region,
     endpoint_url=f"https://{namespace}.compat.objectstorage.{region}.oraclecloud.com"
 )
