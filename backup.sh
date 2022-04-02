a8eef67f2a031d8099fbad4f422158e1ac881ebb
Matt-Mcl <mattmcl2000@gmail.com>
2022-04-02 08:36:59 +0000

    Forwards errors from python to scripts

--- backup.sh
+++ backup.sh
@@ -16,4 +16,10 @@ source "$directory/venv/bin/activate"
 
 python3 $directory/main.py $2 $3 $4 $5 $temp_file
 
+# If the python script has errored, stop start.sh here
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+
 sudo rm $temp_file
--- scripts/mongobackup/mongo_backup.sh
+++ scripts/mongobackup/mongo_backup.sh
@@ -17,5 +17,10 @@ source "$directory/venv/bin/activate"
 
 python3 $directory/main.py $2 $3 $4 $5 "$temp_folder.tar"
 
+# If the python script has errored, stop start.sh here
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 sudo rm -rf $temp_folder
 sudo rm "$temp_folder.tar"
